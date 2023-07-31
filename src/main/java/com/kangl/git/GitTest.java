3ae4503784e6de67fe223b30211f413191f4315e
kangl <1145413114@qq.com>
2023-07-31 22:32:22 +0800

    第一次提交GitTest

---
+++ src/main/java/com/kangl/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.kangl.git;
+/*
+ * @desc   测试Git的代码
+ * @author kangl
+ * @date 2023-07-31 22:27
+ */
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
